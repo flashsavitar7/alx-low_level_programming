3b6d5fc205007f103e89fa069779c1b2b324d77d
Triumph Edet <triumphedet5@gmail.com>
2022-03-10 15:28:57 -0800

    C learning

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
-int main(){
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
-	puts("Programming is like building a multilingual puzzle" "\n");
-        return(0);
+int main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
-
