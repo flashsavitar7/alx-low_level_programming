c7ea637910cc67cca94348875a57e9e2772ad968
Triumph Edet <triumphedet5@gmail.com>
2022-03-12 03:29:54 -0800

    c variables if else while

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+/**
+ * main - prints a combination of three digit numbers
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+
+	int t;
+	int a;
+	int b;
+
+	for (t = 0; t <= 9; t++)
+
+
+	{
+		for (a = t + 1; a <= 9; a++)
+	{
+		for (b = a + 1; b <= 9; b++)
+	{
+		putchar(t + '0');
+		putchar(a + '0');
+		putchar(b + '0');
+
+		if (t < 8)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+	}
+
+	}
+
+
+
+
+	}
+
+		putchar('\n');
+		return (0);
+
+}
