82816fd2a337b33aa313abb3b226cd46da768e6c
Triumph Edet <triumphedet5@gmail.com>
2022-03-15 11:44:44 -0700

    functions and loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - for printing absolute valu of r
+ *
+ *@int: data type
+ *
+ * Return: absolute value
+ */
+
+
+int _abs(int)
+
+{
+	int r;
+
+	if (r >= 0)
+	{
+		return (r);
+	}
+	else
+	{
+		return (r * -1);
+	}
+
+
+}
