83e4c06fb5e282c59aa9d267452a5f3c54be6893
Triumph Edet <triumphedet5@gmail.com>
2022-03-15 08:02:43 -0700

    functions and loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,15 @@
 #include "main.h"
+/**
+ * print_alphabet - for printing lowercase letters
+ *
+ * Return: Always 0 (success)
+ */
 
 void print_alphabet(void)
 
 {
 	int i;
-	
+
 	for (i = 97; i < 123; i++)
 	{
 		_putchar(i);
