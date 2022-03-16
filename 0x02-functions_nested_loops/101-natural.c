d2e97bc8a6c79b97720c0b9ce85a1aa2e3a3e37b
flashsavitar7 <99418944+flashsavitar7@users.noreply.github.com>
2022-03-16 18:25:05 +0100

    functions and loops

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,15 +9,16 @@
 int main(void)
 
 {
-	int a;
+	int a, b;
 
 	for (a = 1; a < 1024; a++)
 
 	{
 		if ((a % 3 == 0) || (a % 5 == 0))
 		{
-			printf("%d\n", a);
+			b += a;
 		}
+		printf("%d\n", b);
 
 	}
 	return (0);
