3738a0ba9c60df279fb55322afa8fc987f9a3697
flashsavitar7 <99418944+flashsavitar7@users.noreply.github.com>
2022-03-16 17:59:23 +0100

    functions and loops

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the add of the Fibonacci numbers
+ *
+ * Return: Always 0.
+ */
+int main(void)
+
+{
+
+	int c;
+	long int a, b, op;
+
+	a = 1;
+	b = 2;
+	printf("%ld, %ld", a, b);
+	for (c = 0; c < 48; c++)
+	{
+		op = a + b;
+		printf(", %ld", fn);
+		a = b;
+		b = op;
+	}
+	printf("\n");
+	return (0);
+}
