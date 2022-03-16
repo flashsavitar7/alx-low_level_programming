b0c5575df6ddf2afadb80b3d94fd6e9e7f783815
Triumph Edet <triumphedet5@gmail.com>
2022-03-15 22:37:47 -0700

    functions and loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* add - for adding two values
+*
+*@a: integer one
+*@b: integer two
+*
+* Return: value
+*/
+
+int add(int a, int b)
+
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints last digit
+ * of a number.
+ *
+ * @n: input integer.
+ *
+ * Return: last digit.
+ */
+
+
+
+int print_last_digit(int n)
+{
+	int l;
+
+	l = n % 10;
+
+	if (l < 0)
+	{
+		_putchar(-l + '0');
+		return (-l);
+	}
+	else
+	{
+		_putchar(l + '0');
+		return (l);
+	}
+}
