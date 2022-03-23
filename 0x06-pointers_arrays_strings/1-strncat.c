90464bf2c5a55f029491ee2712a05f9c1de8c1e7
flashsavitar7 <99418944+flashsavitar7@users.noreply.github.com>
2022-03-24 00:31:51 +0100

    pointer and aaray

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,18 +11,23 @@
  */
 
 
-char *_strncat(char *dest, char *src, int n){
-  
-  
-  int len1,len2,i;
-  len1 = strlen(dest);
-  len2 = strlen(src);
-  
-  for (i = 0; i < n && i <= len2; i++)
-  {
-    dest[len1 + i]= src[i];
-  }
-  return(dest);
+char *_strncat(char *dest, char *src, int n)
+
+{
+ 
+
+	int len1, len2, i;
+
+	len1 = strlen(dest);
+
+	len2 = strlen(src);
+
+	for (i = 0; i < n && i <= len2; i++)
+	{
+		dest[len1 + i] = src[i];
+	}
+
+	return (dest);
 }
 
 
