72baec32655b04eab43131a37e702bc19c194a6a
flashsavitar7 <99418944+flashsavitar7@users.noreply.github.com>
2022-03-23 18:57:41 +0100

    pointer and aaray

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,21 +9,23 @@
  *
  * Return: void
  */
- 
 
 
 char *_strcat(char *dest, char *src)
 
 {
 
-	int len1,len2,i;
+	int len1, len2, i;
+
 	len1 = strlen(dest);
+
 	len2 = strlen(src);
 
 	for (i = 0; i <= len2; i++)
+
 	{
-		dest[len1 + i]= src[i];
+		dest[len1 + i] = src[i];
 	}
-	return(dest);
+	return (dest);
 }
 
