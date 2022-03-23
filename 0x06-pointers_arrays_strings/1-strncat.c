a3d439c0bc7966b6e4cc85413f4ac3ca5786c47b
flashsavitar7 <99418944+flashsavitar7@users.noreply.github.com>
2022-03-23 22:29:37 +0100

    pointer and aaray

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ * @n: n num of bytes
+ * Return: void
+ */
+
+
+char *_strncat(char *dest, char *src, int n)
+
+}
+	int len1, len2, i;
+
+	len1 = strlen(dest);
+
+	len2 = strlen(src);
+
+	for (i = 0; i <= len2; i++)
+
+	{
+		dest[len1 + i] = src[i];
+	}
+
+
+	return (dest);
+}
+
