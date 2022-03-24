d2504b5edc57b7af22d74cb47b0655f6be07fcb1
flashsavitar7 <99418944+flashsavitar7@users.noreply.github.com>
2022-03-24 16:01:37 +0100

    poointers n strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string.
+ * @s2: second string.
+ * Return: 0 if s1 and s2 are equals,
+ * another number if not.
+ */
+
+
+int _strcmp(char *s1, char *s2)
+
+{
+	int i, flag;
+
+	for (i = 0; s1[i] != '\0' ||  s1[i] != '\0'; i++)
+
+	{
+		if (s1[i] != s2[i])
+			flag = s1[i] - s2[i];
+			break;
+	}
+	return (flag);
+
+}
