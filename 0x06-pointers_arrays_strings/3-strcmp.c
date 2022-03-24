afca3a38cdeed3b11e02d63b50bae2d87565de45
flashsavitar7 <99418944+flashsavitar7@users.noreply.github.com>
2022-03-24 17:28:32 +0100

    poointers n strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,16 +12,20 @@
 int _strcmp(char *s1, char *s2)
 
 {
-  int i,flag;
+	int i, flag;
   
-  for(i = 0; s1[i]!='\0' || s2[i]!='\0'; i++){
+	for(i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 
-    if(s1[i] != s2[i])
-      flag = s1[i]- s2[i];
-      break;
-      
+	{
 
-   } 
-return (flag);
+	if(s1[i] != s2[i])
+
+		break;
+
+	}
+	flag = s1[i]- s2[i];
+
+
+	return (flag);
 }
 
